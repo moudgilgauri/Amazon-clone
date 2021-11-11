3cc765be0e75a591126a9be1ad7fa9ddfb0b2ec5
PriyanshuBhala <priyanshu0117.cse19@chitkara.edu.in>
2021-11-11 14:03:54 +0530

    firebase changed

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,15 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDQxn-CrtqMaT33g1BVvkQUHkzO9hO6SlM",
-    authDomain: "clone-3d854.firebaseapp.com",
-    projectId: "clone-3d854",
-    storageBucket: "clone-3d854.appspot.com",
-    messagingSenderId: "773441669715",
-    appId: "1:773441669715:web:d0d3d20b3ad86cb5c854a6",
-    measurementId: "G-MS3F1K8YQ6"
-  };
+  apiKey: "AIzaSyD5zwkVLVtxtPw7ZYqZ5IeRZduAD-eAaG0",
+  authDomain: "clone-3d155.firebaseapp.com",
+  projectId: "clone-3d155",
+  storageBucket: "clone-3d155.appspot.com",
+  messagingSenderId: "315799069284",
+  appId: "1:315799069284:web:b4748177e545b17239f571",
+  measurementId: "G-DXHNM4CMFG"
+};
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-
-  
+export { db,auth};
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,25 @@
 import React,{useState}from 'react'
-import { Link } from "react-router-dom";
+import { Link,useHistory} from "react-router-dom";
 import amazonwhite from "../images/amazonewhite.png"
 import "../css/login.css"
+import { auth } from './firebase';
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const history = useHistory();
 
     const signIn =(e)=>{
         e.preventDefault();
     }
     const Register =(e)=>{
         e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email,password)
+        .then((auth)=>{
+            console.log(auth);
+            history.push('/')
+        })
+        .catch(error=>alert(error.message));
     }
     return (
         <div className="login">
@@ -23,7 +32,7 @@ export default function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" value={email} onChange={e=> setEmail(e.target.value)}/>
+                    <input type="email" value={email} onChange={e=> setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
                     <input type="password" value={password} onChange={e=> setPassword(e.target.value)}/>
